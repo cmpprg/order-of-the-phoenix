0c6534ec215e1173d9221af3ac171e79131162d7
Ryan Camp <campryan@comcast.net>
2020-05-19 09:55:22 -0600

    Write test to check total number of order of phoenix

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -10,16 +10,24 @@ RSpec.describe 'As a user' do
 
       expect(current_path).to eql('/search')
     end
+
+    it "After 'Gryfindor' selection i should see total number of order of phoenix memeber that belong to gryfindor." do
+      visit '/search?house=Gryffindor'
+
+      within '.total' do
+        expect(page).to have_content('Total: 21')
+      end
+    end
   end
 end
 
 # ```
-# As a user,
-# When I visit "/"
-# And I Select “Gryffindor” from the select field
-# (Note: Use the existing select field)
-# And I click "Search For Members“
-# Then I should be on page “/search”
+# As a user, ***
+# When I visit "/" ***
+# And I Select “Gryffindor” from the select field ***
+# (Note: Use the existing select field) ***
+# And I click "Search For Members“ ***
+# Then I should be on page “/search” ***
 # Then I should see the total number of the order of the phoenix members that belong to Gryffindor. (i.e. 21)
 # And I should see a list with the detailed information for the 21 members of the Order of the Phoenix for house Gryffindor.
 # ​
