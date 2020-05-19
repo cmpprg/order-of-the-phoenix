c8e0fcf6eebfcc41da5d0cc89ce6bba86734154e
Ryan Camp <campryan@comcast.net>
2020-05-19 10:33:59 -0600

    Struggling to find information and set up API

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,4 +2,10 @@ class SearchController < ApplicationController
   def index
     @members = HarryPotterService.new.order_of_phoenix_from('gryffindor')
   end
+
+  private
+
+  def gryfindor_order_members
+    OrderMembers.new.return_collection('gryfindor')
+  end
 end
--- app/services/harry_potter_service.rb
+++ app/services/harry_potter_service.rb
@@ -1,5 +1,30 @@
 class HarryPotterService
   def order_of_phoenix_from(house)
+    find_house('Gryffindor')
+    require 'pry'; binding.pry
+    # require 'pry'; binding.pry
+  end
 
+  def all_houses
+    parsed_json('houses')
+  end
+
+  def find_house(house)
+    all_houses.find do |house_info|
+      house_info[:name] = house.capitalize
+    end
+  end
+
+  private
+
+  def connect
+    Faraday.new('https://www.potterapi.com/v1') do |req|
+      req.params['key'] = ENV['HP_KEY']
+    end
+  end
+
+  def parsed_json(url, params = nil)
+    res = connect.get(url, params)
+    JSON.parse(res.body, symbolize_names: true)
   end
 end
